33ddfe5b586514594428cb15cc5fa5a44b51385a
Romina <de.meenta@gmail.com>
2020-06-01 21:05:45 -0400

    Read csv file

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import logging as log
+import sys
+
+import pandas as pd
 
 
-def main():
+def main(argv):
     log.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=log.INFO)
-    log.info("Starting etl")
+
+    if not argv:
+        filename = 'MoviesOnStreamingPlatforms_updated.csv'
+    else:
+        filename = argv[0]
+
+    log.info("Extracting data from file : {}".format(filename))
+    try:
+        movies_df = pd.read_csv(filename)
+    except Exception as e:
+        log.error(e)
+        log.info("Corrected use: python main.py or python main.py [file.csv]")
+
+    log.info(movies_df.head())
+    log.info("Shape: {}".format(movies_df.shape))
+
+    
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
